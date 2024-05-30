52bd3a2696ad07dc65076923ecf6996c80f1fcda
iting0321 <0321iting@gmail.com>
2024-05-30 13:56:46 +0800

    feat:delete function

--- app/src/main/java/com/example/project_ui/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/project_ui/ui/notifications/NotificationsFragment.java
@@ -12,14 +12,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.project_ui.R;
 import com.example.project_ui.databinding.FragmentNotificationsBinding;
 import com.example.project_ui.ui.notifications.Notifications.LanguageRVAdapter;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
+import java.lang.String;
 import java.util.List;
 
 public class NotificationsFragment extends Fragment {
@@ -58,6 +61,48 @@ public class NotificationsFragment extends Fragment {
 
             }
         });
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                // this method is called
+                // when the item is moved.
+                return false;
+            }
+
+            //@Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                // this method is called when we swipe our item to right direction.
+                // on below line we are getting the item at a particular position.
+                String deletedCourse = lngList.get(viewHolder.getAdapterPosition());
+
+                // below line is to get the position
+                // of the item at that position.
+                int position = viewHolder.getAdapterPosition();
+
+                // this method is called when item is swiped.
+                // below line is to remove item from our array list.
+                lngList.remove(viewHolder.getAdapterPosition());
+
+                // below line is to notify our item is removed from adapter.
+                lngRVAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+
+                // below line is to display our snackbar with action.
+                Snackbar.make(recyclerView, deletedCourse, Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // adding on click listener to our action of snack bar.
+                        // below line is to add our item to array list with a position.
+                        lngList.add(position, deletedCourse);
+
+                        // below line is to notify item is
+                        // added to our adapter class.
+                        lngRVAdapter.notifyItemInserted(position);
+                    }
+                }).show();
+            }
+            // at last we are adding this
+            // to our recycler view.
+        }).attachToRecyclerView(recyclerView);
         return view;
 
     }
